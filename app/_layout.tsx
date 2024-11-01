c6a3479a8072592bf9935475c7dd1bbf8731254c
nikzaim <156214902+nikzaim@users.noreply.github.com>
2024-11-01 11:49:01 +0800

    Setup Sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Slot, Stack, useRouter, useSegments } from "expo-router";
-import { ClerkProvider, ClerkLoaded, useAuth } from "@clerk/clerk-expo";
+import { Slot, Stack, useNavigationContainerRef, useRouter, useSegments } from "expo-router";
+import { ClerkProvider, ClerkLoaded, useAuth, useUser } from "@clerk/clerk-expo";
 import { tokenCache } from "@/utils/cache";
 import { useFonts, DMSans_400Regular, DMSans_500Medium, DMSans_700Bold } from "@expo-google-fonts/dm-sans";
 import { useEffect } from "react";
@@ -7,6 +7,8 @@ import * as SplashScreen from "expo-splash-screen";
 
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import * as Sentry from "@sentry/react-native";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
@@ -17,6 +19,30 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 if (!publishableKey) {
   throw new Error("Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env");
 }
+
+// const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  attachScreenshot: true,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  tracesSampleRate: 1.0,
+  _experiments: {
+    // Here, we'll capture profiles for 100% of transactions.
+    profilesSampleRate: 1.0,
+    // Session replays
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
+  },
+  integrations: [
+    // new Sentry.ReactNativeTracing({
+    //   // Pass instrumentation to be used as `routingInstrumentation`
+    //   routingInstrumentation,
+    //   enableNativeFramesTracking: true,
+    // }),
+    Sentry.mobileReplayIntegration(),
+  ],
+});
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,6 +56,7 @@ const InitialLayout = () => {
   const { isLoaded, isSignedIn } = useAuth();
   const segments = useSegments();
   const router = useRouter();
+  const user = useUser();
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -48,12 +75,29 @@ const InitialLayout = () => {
       router.replace("/(public)");
     }
   }, [isSignedIn]);
+
+  useEffect(() => {
+    if (user && user.user) {
+      Sentry.setUser({ email: user.user.emailAddresses[0].emailAddress, id: user.user.id });
+    } else {
+      Sentry.setUser(null);
+    }
+  }, [user]);
+
   return <Slot />;
 };
 
-export default function RootLayout() {
+const RootLayoutNav = () => {
+  // const ref = useNavigationContainerRef();
+
+  // useEffect(() => {
+  //   if (ref) {
+  //     routingInstrumentation.registerNavigationContainer(ref);
+  //   }
+  // }, [ref]);
+
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <ClerkProvider publishableKey={publishableKey!} tokenCache={tokenCache}>
       <ClerkLoaded>
         <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
           <InitialLayout />
@@ -61,4 +105,6 @@ export default function RootLayout() {
       </ClerkLoaded>
     </ClerkProvider>
   );
-}
+};
+
+export default Sentry.wrap(RootLayoutNav);
